147252b1b146568c2925f50e310e8933565f265c
Abby <abigaillyons@college.harvard.edu>
2017-03-25 14:40:15 -0400

    class for ml

--- ml.py
+++ ml.py
@@ -2,14 +2,21 @@ import numpy as np
 from sklearn import svm
 from sklearn.externals import joblib
 
-# Do initial training on an account
-def fit(charges):
-	# train and pickle our data
-	clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
-	clf.fit(charges)
-	joblib.dump(clf, 'train.pkl')
+class Svm(object):
+	def __init__(self, filename):
+		self.file = filename
+		if os.path.isfile(filename):
+			self.clf = joblib.load(filename)
+		else:
+			self.clf = None
 
-# Classify a new charge
-def classify_new(new_charge):
-	clf = joblib.load('train.pkl') 
-	return clf.predict([new_charge])  # if prediction is -1, send out charge
+    def refit(self, charges):
+    	self.clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
+    	self.clf.fit(charges)
+    	joblib.dump(clf, self.file)
+
+	def classify_new(self, new_charge):
+		if self.clf == None:
+			print "Tried to classify with no model"
+		else:
+			return self.clf.predict([new_charge])
