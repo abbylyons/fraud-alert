826f3978e8f3628fd415d2c5d484c41e3ecb71d0
timothy.tamm <tamm@college.harvard.edu>
2017-03-25 14:27:19 -0400

    setup

--- Nessi/__init__.py
+++
@@ -1,1 +0,0 @@
-from Nessi import Nessi
--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python
 
 import json
+import pickle
+from NessiWrapper import Nessi
 
-config = {}
+apiKey = '';
+customerId = '';
+nessi = None
 
-def main():
+def get
+
+def setup():
     with open('config.json') as json_data:
         config = json.load(json_data)
+        apiKey = config['nessi_key']
+        customerId = config['customer_id']
+    nessi = Nessi('customer', apiKey)
 
+
+def main():
+    setup()
+    
 if __name__ == "__main__":
         main()
